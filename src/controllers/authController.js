f33bf760427202db652cb4f7233aa9d87a2ec9ae
augustinegrepo <augustinegrepo@gmail.com>
2026-07-10 21:11:23 +0800

    Built User Functions Save Login and JWT Features

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,15 +1,17 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const registerUser = async (req, res) => {
-    const { firstName, lastName, email, password } = req.body;
+  const { firstName, lastName, email, password } = req.body;
 
-    // Validdate required fields
+  try {
+    // Validate required fields
     if (!firstName || !lastName || !email || !password) {
-        return res.status(400).json({
-            success: false,
-            message: "All fields are required"
-        });
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
     }
 
     // Email validation
@@ -17,34 +19,122 @@ const registerUser = async (req, res) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (!emailRegex.test(email)) {
-        return res.status(400).json({
-            success: false,
-            message: "Please enter a valid email address."
-        });
+      return res.status(400).json({
+        success: false,
+        message: "Please enter a valid email address.",
+      });
     }
 
-     // Duplicate email check
+    // Duplicate email check
 
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-        return res.status(409).json({
-            success: false,
-            message: "Email is already registered."
-        });
+      return res.status(409).json({
+        success: false,
+        message: "Email is already registered.",
+      });
     }
 
-    // Hashing the password 
+    // Hashing the password
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    return res.status(200).json({
-        success: true,
-        message: "Password hashed successfully.",
-        hashedPassword
+    // Create new user
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
     });
+
+    // save user to database
+    await newUser.save();
+
+    // Return success response
+
+    return res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Validate required fields
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and password are required.",
+      });
+    }
+
+    // Find user by email
+    const user = await User.findOne({ email });
+
+    // User not found
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password.",
+      });
+    }
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    // Wrong password
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password.",
+      });
+    }
+
+    // Password correct
+    // return res.status(200).json({
+    //   success: true,
+    //   message: "Password verified. Ready to generate JWT.",
+    // });
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      },
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Login successful.",
+      token,
+    });
+  } catch (error) {
+    console.error("Login Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
 };
 
 module.exports = {
-    registerUser
+  registerUser,
+  loginUser,
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const controller = require("../controllers/authController");
+const {registerUser, loginUser} = require("../controllers/authController");
 
-console.log(controller);
-console.log(typeof controller.registerUser);
+// Register
+router.post("/register", registerUser);
 
-router.post("/register", controller.registerUser);
+// Login
+router.post("/login", loginUser);
 
 module.exports = router;
